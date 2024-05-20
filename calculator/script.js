ae3a7b2c39afbc3891d40ae3ba640dd48d8c7670
bisht65190 <bisht65190@gmail.com>
2024-05-20 10:56:42 +0530

    complete calculator

--- calculator/script.js
+++ calculator/script.js
@@ -5,20 +5,36 @@ let operators=document.querySelectorAll(".operator")
 let clearButton=document.querySelector(".clear")
 let calculateBtn=document.querySelector(".calculate")
 
-
-// console.log(resultInput,"resultInput");
-// console.log(buttons,"buttons");
-// console.log(operators,"operators");
-// console.log(clearButton,"clearButton");
-// console.log(calculateBtn,"calculateBtn");
-
 buttons.forEach((button)=>{
 console.log(button);
 button.addEventListener("click",()=>{
     resultInput.value=resultInput.value+button.innerText
-    console.log(resultInput.value,"resultInput.value");
+    // console.log(resultInput.value,"resultInput.value");
 })
 })
+operators.forEach((operator)=>{
+    operator.addEventListener("click",()=>{
+        resultInput.value=resultInput.value+operator.innerText
+        // console.log(resultInput.value,"resultInput.value");
+    })
+})
+calculateBtn.addEventListener("click",()=>{
+try {
+    // throw new error("error")
+    // let calculatedResult=eval(resultInput.value)
+    // resultInput.value=calculatedResult 
+} catch (error) {
+    resultInput.value="sibla" 
+}
+})
+
+
+clearButton.addEventListener("click",()=>{
+    resultInput.value="" 
+})
+
+
+
 
 
 
